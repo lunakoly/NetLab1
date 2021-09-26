185ef097e5947accc663dc3da16fe6cce3a06d41
Nick Lunyak <lunyak.kolya@mail.ru>
2021-09-26 13:57:14 +0300

    Remove the buffer from CappedReader

--- shared/src/capped_reader.rs
+++ shared/src/capped_reader.rs
@@ -8,8 +8,6 @@ pub const CAPPED_READER_CAPACITY: usize = 100;
 
 pub struct CappedReader<R> {
     stream: R,
-    buffer: [u8; CAPPED_READER_CAPACITY],
-    length: usize,
     offset: usize,
 }
 
@@ -21,8 +19,6 @@ impl<R: Read> IntoCappedReader<R> for R {
     fn capped(self) -> CappedReader<R> {
         CappedReader {
             stream: self,
-            buffer: [0; CAPPED_READER_CAPACITY],
-            length: 0,
             offset: 0,
         }
     }
@@ -30,44 +26,28 @@ impl<R: Read> IntoCappedReader<R> for R {
 
 impl<R: Read> Read for CappedReader<R> {
     fn read(&mut self, buffer: &mut [u8]) -> Result<usize, std::io::Error> {
-        if self.offset == self.length {
-            if self.length < CAPPED_READER_CAPACITY {
-                self.length += self.stream.read(&mut self.buffer[self.length..])?;
-            } else {
-                return Err(std::io::ErrorKind::InvalidData.into());
-            }
-
-            // Only possible if we've read 0
-            // bytes from the underlying stream
-            if self.offset == self.length {
-                return Ok(0);
-            }
-        }
-
-        let copy_count = std::cmp::min(self.length - self.offset, buffer.len());
-        let tail = self.offset + copy_count;
-
-        let copy_result = std::io::copy(
-            &mut &self.buffer[self.offset..tail],
-            &mut &mut buffer[..copy_count]
+        let max_allowed_count = std::cmp::min(
+            CAPPED_READER_CAPACITY - self.offset,
+            buffer.len()
         );
 
-        match copy_result {
-            Ok(actually_copied) => {
-                self.offset += actually_copied as usize;
-                Ok(actually_copied as usize)
-            }
-            Err(error) => {
-                Err(error)
-            }
+        let read_count = if max_allowed_count > 0 {
+            self.stream.read(&mut buffer[..max_allowed_count])?
+        } else {
+            return Err(std::io::ErrorKind::InvalidData.into());
+        };
+
+        if read_count == 0 {
+            return Ok(0);
         }
+
+        self.offset += read_count;
+        Ok(read_count)
     }
 }
 
 impl<R: Read> CappedRead for CappedReader<R> {
     fn clear(&mut self) {
-        self.length -= self.offset;
-        self.buffer.rotate_left(self.offset);
         self.offset = 0;
     }
 }
